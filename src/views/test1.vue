da32f69b1d72a528d0de813c7767cf6d3b219451
bianjunping <bianjunping@oilchem.net>
2021-06-30 13:38:14 +0800

    这是test1
    这是第二次提交
    
    添加89

---
+++ src/views/test1.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: bjp
+ * @Description: 这是test1
+-->
+<template>
+  <div class="test1">
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>第二次提交插入的</li>
+      <li>3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test1',
+  components: {},
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/test2.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: bjp
+ * @Description: 这是test2
+-->
+<template>
+  <div class="test2">
+    <ul>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>8</li>
+      <li>9</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test2',
+  components: {},
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
