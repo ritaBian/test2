fbf5803901dce84fbdf6ebda4f0261e323978d00
bianjunping <bianjunping@oilchem.net>
2021-06-30 13:38:14 +0800

    这是test1

---
+++ src/views/test1.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: bjp
+ * @Description: 这是test1
+-->
+<template>
+ <div class="test1">
+   <ul>
+     <li>1</li>
+     <li>2</li>
+     <li>3</li>
+   </ul>
+ </div>
+</template>
+
+<script>
+export default {
+ name: 'test1',
+ components: {},
+ data() {
+ return {
+
+ }
+ },
+ mounted() {},
+ methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
